46041d0d50ee139717ca71fd474802b2384235c4
Torsten Kleiber <torsten.kleiber@web.de>
2022-03-24 13:24:12 +0100

    Test Docker Compose file with JUnit 5 and TestContainers Framework

---
+++ src/test/java/TestJenkins.java
@@ -0,0 +1,53 @@
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.shaded.org.hamcrest.MatcherAssert;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.testcontainers.shaded.org.hamcrest.Matchers.containsString;
+
+@DisplayName("Jenkins")
+public class TestJenkins {
+
+  public static final int JENKINS_PORT = 8080;
+
+  @ClassRule
+  public static DockerComposeContainer jenkinsServer =
+      new DockerComposeContainer(new File("docker-compose.yml"))
+          .withExposedService("jenkins", JENKINS_PORT)
+          .waitingFor("jenkins", Wait.forLogMessage(".*Jenkins is fully up and running*\\n", 1));
+
+  @Test
+  @DisplayName("first request to fresh container should return \"Authentication required\"")
+  public void givenFreshContainer_whenFirstRequest_thenReturnsAuthenticationRequired()
+      throws Exception {
+    String jenkinsUrl = "http://"
+        + jenkinsServer.getServiceHost("jenkins", JENKINS_PORT)
+        + ":" +
+        jenkinsServer.getServicePort("jenkins", JENKINS_PORT);
+
+    MatcherAssert.assertThat("first request to fresh container should return \"Authentication required\"",
+        callHttpEndpoint(jenkinsUrl),
+        containsString("Authentication required")
+    );
+  }
+
+  private static String callHttpEndpoint(String url) throws IOException, InterruptedException {
+    HttpClient httpClient = HttpClient.newBuilder().build();
+    HttpRequest mainRequest = HttpRequest.newBuilder()
+        .uri(URI.create(url))
+        .build();
+
+    HttpResponse<String> response = httpClient.send(mainRequest, HttpResponse.BodyHandlers.ofString());
+    return response.body();
+  }
+
+}
